a18cfb7ac64f032ec603079dd778a9a11992851e
Yang Chen <ychescale9@gmail.com>
2020-01-14 01:06:53 +1100

    Move unneeded plugin dependencies out of buildSrc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,10 @@ buildscript {
     dependencies {
         classpath(build.getValue("androidGradlePlugin"))
         classpath(build.getValue("kotlinGradlePlugin"))
+        classpath(build.getValue("kotlinSerializationPlugin"))
         classpath(build.getValue("googleServicesGradlePlugin"))
         classpath(build.getValue("dexcountGradlePlugin"))
+        classpath(build.getValue("sqlDelightGradlePlugin"))
     }
 }
 
