1d95ae73938b040b71e2cc1002c4d3dd22a6fc72
Yang Chen <ychescale9@gmail.com>
2020-01-22 15:13:04 +1100

    Fix packaging options for tests.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/BaselineProjectConfigs.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/BaselineProjectConfigs.kt
@@ -95,6 +95,10 @@ internal fun BaseExtension.configureCommonAndroidOptions() {
 internal fun LibraryExtension.configureAndroidLibraryOptions() {
     // Disable generating BuildConfig.java
     buildFeatures.buildConfig = false
+
+    packagingOptions {
+        exclude("META-INF/*.kotlin_module")
+    }
 }
 
 /**
@@ -118,7 +122,8 @@ internal fun AppExtension.configureAndroidApplicationOptions(project: Project) {
         excludes = setOf(
             "META-INF/*.kotlin_module",
             "META-INF/proguard/coroutines.pro",
-            "META-INF/MANIFEST.MF"
+            "META-INF/MANIFEST.MF",
+            "LICENSE.txt"
         )
     }
 }
