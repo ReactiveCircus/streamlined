99ff6bc7a64cbaecff0db6096a0179aaacb2de99
Yang Chen <ychescale9@gmail.com>
2020-01-31 11:14:36 +1100

    Build tools 29.0.3.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/AndroidSdk.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/AndroidSdk.kt
@@ -6,5 +6,5 @@ internal object androidSdk {
     const val minSdk = 23
     const val targetSdk = 29
     const val compileSdk = 29
-    const val buildTools = "29.0.2"
+    const val buildTools = "29.0.3"
 }
