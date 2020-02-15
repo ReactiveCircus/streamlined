b7c0b0425cffc302f3900b6e88c2211989b234b7
Yang Chen <ychescale9@gmail.com>
2020-02-15 15:27:52 +1100

    Enable app module compile time R class generation.

--- app/src/main/java/io/github/reactivecircus/streamlined/StreamlinedActivity.kt
+++ app/src/main/java/io/github/reactivecircus/streamlined/StreamlinedActivity.kt
@@ -2,12 +2,13 @@ package io.github.reactivecircus.streamlined
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.github.reactivecircus.streamlined.ui.R as ThemeR
 
 class StreamlinedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         supportFragmentManager.fragmentFactory = appComponent.fragmentFactory
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_Streamlined_DayNight)
+        setTheme(ThemeR.style.Theme_Streamlined_DayNight)
         setContentView(R.layout.activity_streamlined)
     }
 }
