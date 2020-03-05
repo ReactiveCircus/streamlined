26b278e6f7fe528b51ab0838697ff8532bb42d0a
Yang Chen <ychescale9@gmail.com>
2020-03-05 18:46:35 +1100

    Update date formatter to produce consistent result in Java 9+.

--- ui-common/src/main/java/io/github/reactivecircus/streamlined/ui/util/DateExtensions.kt
+++ ui-common/src/main/java/io/github/reactivecircus/streamlined/ui/util/DateExtensions.kt
@@ -3,11 +3,12 @@ package io.github.reactivecircus.streamlined.ui.util
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 /**
  * Converts the timestamp to a formatted String
  */
 fun Long.toFormattedDateString(pattern: String): String {
-    val formatter = DateTimeFormatter.ofPattern(pattern)
+    val formatter = DateTimeFormatter.ofPattern(pattern).withLocale(Locale.ENGLISH)
     return Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).format(formatter)
 }
