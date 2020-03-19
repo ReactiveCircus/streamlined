e82955ac1dd0b3abc6a5880daa0b6f7b451a0c2c
Yang Chen <ychescale9@gmail.com>
2020-03-19 22:31:00 +1100

    Update AndroidX, truth.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
@@ -26,15 +26,15 @@ object versions {
         const val core = "1.3.0-alpha02"
         const val annotation = "1.1.0"
         const val appCompat = "1.2.0-alpha03"
-        const val activity = "1.2.0-alpha01"
-        const val fragment = "1.3.0-alpha01"
+        const val activity = "1.2.0-alpha02"
+        const val fragment = "1.3.0-alpha02"
         const val coordinatorLayout = "1.1.0"
         const val recyclerView = "1.2.0-alpha01"
         const val swipeRefreshLayout = "1.1.0-beta01"
         const val constraintLayout = "1.1.3"
         const val arch = "2.1.0"
         const val lifecycle = "2.3.0-alpha01"
-        const val navigation = "2.3.0-alpha03"
+        const val navigation = "2.3.0-alpha04"
         const val work = "2.4.0-alpha01"
 
         object test {
@@ -64,7 +64,7 @@ object versions {
     const val timber = "4.7.1"
     const val junit = "4.13"
     const val mockk = "1.9.3"
-    const val truth = "1.0"
+    const val truth = "1.0.1"
 }
 
 object libraries {
