aa4b047190d7016db161d45e6e089d1c00e6e0fc
Yang <ychescale9@gmail.com>
2020-06-06 21:14:58 +1000

    Replace mocks with fakes in domain module tests.

--- domain/src/test/java/io/github/reactivecircus/streamlined/domain/interactor/FetchPersonalizedStoriesTest.kt
+++ domain/src/test/java/io/github/reactivecircus/streamlined/domain/interactor/FetchPersonalizedStoriesTest.kt
@@ -1,12 +1,7 @@
 package io.github.reactivecircus.streamlined.domain.interactor
 
 import com.google.common.truth.Truth.assertThat
-import io.github.reactivecircus.streamlined.domain.model.Story
-import io.github.reactivecircus.streamlined.domain.repository.StoryRepository
 import io.github.reactivecircus.streamlined.domain.testCoroutineDispatcherProvider
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
@@ -14,30 +9,7 @@ import org.junit.Test
 @ExperimentalCoroutinesApi
 class FetchPersonalizedStoriesTest {
 
-    private val dummyPersonalizedStoryList = listOf(
-        Story(
-            id = 1,
-            source = "source1",
-            title = "Article 1",
-            author = "Yang",
-            description = "Description...",
-            url = "url",
-            imageUrl = "image-url",
-            publishedTime = 1000L
-        ),
-        Story(
-            id = 2,
-            source = "source2",
-            title = "Article 2",
-            author = "Yang",
-            description = "Description...",
-            url = "url",
-            imageUrl = "image-url",
-            publishedTime = 2000L
-        )
-    )
-
-    private val storyRepository = mockk<StoryRepository>()
+    private val storyRepository = FakeStoryRepository()
 
     private val fetchPersonalizedStories = FetchPersonalizedStories(
         storyRepository = storyRepository,
@@ -47,14 +19,11 @@ class FetchPersonalizedStoriesTest {
     @Test
     fun `personalized stories fetched from repository are returned`() = runBlockingTest {
         val query = "query"
-        coEvery { storyRepository.fetchPersonalizedStories(query) } returns dummyPersonalizedStoryList
 
         val result = fetchPersonalizedStories
             .execute(FetchPersonalizedStories.Params(query))
 
         assertThat(result)
-            .isEqualTo(dummyPersonalizedStoryList)
-
-        coVerify { storyRepository.fetchPersonalizedStories(query) }
+            .isEqualTo(FakeStoryRepository.DUMMY_PERSONALIZED_STORY_LIST)
     }
 }
--- domain/src/test/java/io/github/reactivecircus/streamlined/domain/interactor/GetStoryByIdTest.kt
+++ domain/src/test/java/io/github/reactivecircus/streamlined/domain/interactor/GetStoryByIdTest.kt
@@ -2,30 +2,15 @@ package io.github.reactivecircus.streamlined.domain.interactor
 
 import com.google.common.truth.Truth.assertThat
 import io.github.reactivecircus.coroutines.test.ext.assertThrows
-import io.github.reactivecircus.streamlined.domain.model.Story
-import io.github.reactivecircus.streamlined.domain.repository.StoryRepository
 import io.github.reactivecircus.streamlined.domain.testCoroutineDispatcherProvider
-import io.mockk.coEvery
-import io.mockk.coVerifyAll
-import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class GetStoryByIdTest {
-    private val dummyStory = Story(
-        id = 1,
-        source = "source",
-        title = "Article 1",
-        author = "Yang",
-        description = "Description...",
-        url = "url",
-        imageUrl = "image-url",
-        publishedTime = 1000L
-    )
 
-    private val storyRepository = mockk<StoryRepository>()
+    private val storyRepository = FakeStoryRepository()
 
     private val getStoryById = GetStoryById(
         storyRepository = storyRepository,
@@ -34,21 +19,15 @@ class GetStoryByIdTest {
 
     @Test
     fun `getStoryById from repository when story is found`() = runBlockingTest {
-        val id = 1L
-        coEvery { storyRepository.getStoryById(id) } returns dummyStory
+        val id = FakeStoryRepository.DUMMY_HEADLINE_STORY_LIST[0].id
 
         assertThat(getStoryById.execute(GetStoryById.Params(id)))
-            .isEqualTo(dummyStory)
-
-        coVerifyAll {
-            storyRepository.getStoryById(id)
-        }
+            .isEqualTo(FakeStoryRepository.DUMMY_HEADLINE_STORY_LIST[0])
     }
 
     @Test
     fun `getStoryById from repository when story is not found`() = runBlockingTest {
-        val id = 1L
-        coEvery { storyRepository.getStoryById(id) } returns null
+        val id = 100L
 
         val exception = assertThrows<NoSuchElementException> {
             getStoryById.execute(GetStoryById.Params(id))
@@ -57,9 +36,5 @@ class GetStoryByIdTest {
         assertThat(exception)
             .hasMessageThat()
             .isEqualTo("Could not find story for id $id")
-
-        coVerifyAll {
-            storyRepository.getStoryById(id)
-        }
     }
 }
