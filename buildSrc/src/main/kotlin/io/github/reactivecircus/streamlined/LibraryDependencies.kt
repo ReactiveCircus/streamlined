b851dad36758b2b66d5fa6418a28028d822d5706
Yang <ychescale9@gmail.com>
2020-07-24 17:10:10 +1000

    MDC 1.3.0-alpha02.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
@@ -52,7 +52,7 @@ object versions {
         }
     }
 
-    const val material = "1.3.0-alpha01"
+    const val material = "1.3.0-alpha02"
     const val insetter = "0.3.0"
     const val dagger = "2.28"
     const val assistedInject = "0.5.2"
