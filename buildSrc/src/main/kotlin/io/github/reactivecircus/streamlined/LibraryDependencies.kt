ed0d844e13d9d8bd000593c721f36f6cd0f8e1c8
Yang <ychescale9@gmail.com>
2020-09-14 09:49:57 +1000

    deteket 1.13.1.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
@@ -3,7 +3,7 @@
 package io.github.reactivecircus.streamlined
 
 object versions {
-    const val detekt = "1.12.0"
+    const val detekt = "1.13.1"
     const val bugsnag = "5.1.0"
     const val leakCanary = "2.4"
     const val desugarLibs = "1.0.10"
