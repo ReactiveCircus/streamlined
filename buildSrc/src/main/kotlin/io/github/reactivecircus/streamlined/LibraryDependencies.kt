cb9ed33dfceef54533a0b2ca52d2be2a2af6db5c
Yang <ychescale9@gmail.com>
2020-10-02 13:54:51 +1000

    Update AndroidX, MDC.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
@@ -22,16 +22,16 @@ object versions {
     const val workflow = "1.0.0-alpha.4"
 
     object androidx {
-        const val core = "1.5.0-alpha03"
+        const val core = "1.5.0-alpha04"
         const val annotation = "1.2.0-alpha01"
         const val appCompat = "1.3.0-alpha02"
-        const val activity = "1.2.0-alpha08"
-        const val fragment = "1.3.0-alpha08"
+        const val activity = "1.2.0-beta01"
+        const val fragment = "1.3.0-beta01"
         const val coordinatorLayout = "1.1.0"
-        const val recyclerView = "1.2.0-alpha05"
+        const val recyclerView = "1.2.0-alpha06"
         const val swipeRefreshLayout = "1.2.0-alpha01"
         const val constraintLayout = "2.0.1"
-        const val lifecycle = "2.3.0-alpha07"
+        const val lifecycle = "2.3.0-beta01"
         const val navigation = "2.3.0"
         const val work = "2.5.0-alpha02"
 
@@ -50,7 +50,7 @@ object versions {
         }
     }
 
-    const val material = "1.3.0-alpha02"
+    const val material = "1.3.0-alpha03"
     const val insetter = "0.3.0"
     const val dagger = "2.29"
     const val assistedInject = "0.6.0"
