a16cc49d005317252b7f6f3157e0387737b79b3f
Yang <ychescale9@gmail.com>
2020-12-10 13:15:50 +1100

    Bugsnag 5.3.1.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
@@ -4,7 +4,7 @@ package io.github.reactivecircus.streamlined
 
 object versions {
     const val detekt = "1.14.2"
-    const val bugsnag = "5.2.3"
+    const val bugsnag = "5.3.1"
     const val leakCanary = "2.5"
     const val desugarLibs = "1.0.10"
 
