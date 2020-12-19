6e89525af148a8552d979621feaaef42720145e6
Yang <ychescale9@gmail.com>
2020-12-19 12:12:54 +1100

    detekt 1.15.0.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
@@ -3,7 +3,7 @@
 package io.github.reactivecircus.streamlined
 
 object versions {
-    const val detekt = "1.14.2"
+    const val detekt = "1.15.0"
     const val bugsnag = "5.3.1"
     const val leakCanary = "2.5"
     const val desugarLibs = "1.0.10"
