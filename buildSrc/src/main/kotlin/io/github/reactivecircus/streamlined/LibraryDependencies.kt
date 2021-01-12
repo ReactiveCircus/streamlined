9d5b4df33bbe565dc4fcff5721e4eeef4ea31669
Yang <ychescale9@gmail.com>
2021-01-12 13:56:32 +1100

    Coil 1.1.1.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
@@ -59,7 +59,7 @@ object versions {
     const val retrofitSerializationConverter = "0.8.0"
     const val sqldelight = "1.4.4"
     const val store = "4.0.0-alpha07"
-    const val coil = "1.1.0"
+    const val coil = "1.1.1"
     const val timber = "4.7.1"
     const val junit = "4.13.1"
     const val truth = "1.1"
