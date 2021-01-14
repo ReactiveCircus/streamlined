c4887d476583a1d942f210e65ee974794963799f
Yang <ychescale9@gmail.com>
2021-01-14 13:03:29 +1100

    Update AndroidX.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/LibraryDependencies.kt
@@ -22,9 +22,9 @@ object versions {
     const val workflow = "1.0.0-alpha.9"
 
     object androidx {
-        const val core = "1.5.0-alpha05"
-        const val annotation = "1.2.0-alpha01"
-        const val appCompat = "1.3.0-alpha02"
+        const val core = "1.5.0-beta01"
+        const val annotation = "1.2.0-beta01"
+        const val appCompat = "1.3.0-beta01"
         const val activity = "1.2.0-rc01"
         const val fragment = "1.3.0-rc01"
         const val coordinatorLayout = "1.1.0"
@@ -33,7 +33,7 @@ object versions {
         const val constraintLayout = "2.0.4"
         const val lifecycle = "2.3.0-rc01"
         const val navigation = "2.3.2"
-        const val work = "2.5.0-beta02"
+        const val work = "2.5.0-rc01"
 
         object test {
             const val core = "1.3.1-alpha02"
