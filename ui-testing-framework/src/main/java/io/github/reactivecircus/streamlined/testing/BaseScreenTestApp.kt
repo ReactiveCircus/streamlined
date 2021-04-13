6f4429e4bf62f1965b2cb08f7eb3fe1635bef35b
Yang <ychescale9@gmail.com>
2021-04-13 15:16:31 +1000

    Fix fake image loader.

--- ui-testing-framework/src/main/java/io/github/reactivecircus/streamlined/testing/BaseScreenTestApp.kt
+++ ui-testing-framework/src/main/java/io/github/reactivecircus/streamlined/testing/BaseScreenTestApp.kt
@@ -1,6 +1,7 @@
 package io.github.reactivecircus.streamlined.testing
 
 import android.app.Application
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import coil.Coil.setImageLoader
@@ -25,47 +26,50 @@ abstract class BaseScreenTestApp : Application() {
         Timber.plant(TestDebugTree())
 
         // set default image loader
-        setImageLoader(FakeImageLoader)
+        setImageLoader(fakeImageLoader)
     }
 }
 
-private val FakeImageLoader = object : ImageLoader {
+private val Context.fakeImageLoader: ImageLoader
+    get() = object : ImageLoader {
 
-    private val drawable = ColorDrawable(Color.TRANSPARENT)
+        private val drawable = ColorDrawable(Color.TRANSPARENT)
 
-    private val disposable = object : Disposable {
-        override val isDisposed = true
+        private val disposable = object : Disposable {
+            override val isDisposed = true
 
-        @OptIn(ExperimentalCoilApi::class)
-        override suspend fun await() = Unit
+            @OptIn(ExperimentalCoilApi::class)
+            override suspend fun await() = Unit
 
-        override fun dispose() = Unit
-    }
+            override fun dispose() = Unit
+        }
 
-    override val defaults = DefaultRequestOptions()
+        override val defaults = DefaultRequestOptions()
 
-    override val memoryCache get() = throw UnsupportedOperationException()
+        override val memoryCache get() = throw UnsupportedOperationException()
 
-    override val bitmapPool = BitmapPool(0)
+        override val bitmapPool = BitmapPool(0)
 
-    override fun enqueue(request: ImageRequest): Disposable {
-        request.target?.onStart(drawable)
-        request.target?.onSuccess(drawable)
-        return disposable
-    }
+        override fun enqueue(request: ImageRequest): Disposable {
+            request.target?.onStart(drawable)
+            request.target?.onSuccess(drawable)
+            return disposable
+        }
 
-    override suspend fun execute(request: ImageRequest): ImageResult {
-        return SuccessResult(
-            drawable = drawable,
-            request = request,
-            metadata = ImageResult.Metadata(
-                memoryCacheKey = MemoryCache.Key(""),
-                isSampled = false,
-                dataSource = DataSource.MEMORY_CACHE,
-                isPlaceholderMemoryCacheKeyPresent = false
+        override suspend fun execute(request: ImageRequest): ImageResult {
+            return SuccessResult(
+                drawable = drawable,
+                request = request,
+                metadata = ImageResult.Metadata(
+                    memoryCacheKey = MemoryCache.Key(""),
+                    isSampled = false,
+                    dataSource = DataSource.MEMORY_CACHE,
+                    isPlaceholderMemoryCacheKeyPresent = false
+                )
             )
-        )
-    }
+        }
 
-    override fun shutdown() = Unit
-}
+        override fun shutdown() = Unit
+
+        override fun newBuilder() = ImageLoader.Builder(this@fakeImageLoader)
+    }
