0c7806694fbaa3b9e0d76ac3a5aad885dafbc2af
Yang <ychescale9@gmail.com>
2021-05-30 17:21:03 +1000

    JUnit 4.13.2.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
@@ -67,7 +67,7 @@ object Versions {
     const val store = "4.0.0-alpha07"
     const val coil = "1.2.1"
     const val timber = "4.7.1"
-    const val junit = "4.13.1"
+    const val junit = "4.13.2"
     const val truth = "1.1.3"
     const val radiography = "2.2.0"
 }
