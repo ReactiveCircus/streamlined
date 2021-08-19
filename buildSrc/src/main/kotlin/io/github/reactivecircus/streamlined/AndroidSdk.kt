49d93df2768d161fa602fe5308cb6234130fc3fd
Yang <ychescale9@gmail.com>
2021-08-19 23:47:51 +1000

    Build tools 31.0.0.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/AndroidSdk.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/AndroidSdk.kt
@@ -6,5 +6,5 @@ internal object androidSdk {
     const val minSdk = 23
     const val targetSdk = 31
     const val compileSdk = 31
-    const val buildTools = "30.0.3"
+    const val buildTools = "31.0.0"
 }
