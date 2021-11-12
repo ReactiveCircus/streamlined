f0c8e5a2b8df8ca5f8ec2f442c5e2b40f49ddb71
Yang <ychescale9@gmail.com>
2021-11-12 13:24:33 +1100

    Serialization 1.3.1. Dagger 2.40.1.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
@@ -17,7 +17,7 @@ object Versions {
 
     object kotlinx {
         const val coroutines = "1.5.2"
-        const val serialization = "1.3.0"
+        const val serialization = "1.3.1"
     }
 
     object firebase {
@@ -60,7 +60,7 @@ object Versions {
 
     const val material = "1.4.0-rc01"
     const val insetter = "0.4.0"
-    const val hilt = "2.40"
+    const val hilt = "2.40.1"
     const val okhttp = "4.9.1"
     const val retrofit = "2.9.0"
     const val retrofitSerializationConverter = "0.8.0"
