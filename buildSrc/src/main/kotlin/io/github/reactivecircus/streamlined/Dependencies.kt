22ee81c2b961657307b43688b268634da35e2102
Yang <ychescale9@gmail.com>
2021-11-18 14:07:15 +1100

    Update AndroidX.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
@@ -31,16 +31,16 @@ object Versions {
     object androidx {
         const val core = "1.7.0"
         const val annotation = "1.3.0"
-        const val appCompat = "1.4.0-alpha03"
+        const val appCompat = "1.4.0"
         const val activity = "1.4.0"
-        const val fragment = "1.4.0-beta01"
+        const val fragment = "1.4.0"
         const val coordinatorLayout = "1.1.0"
         const val recyclerView = "1.2.1"
         const val swipeRefreshLayout = "1.2.0-alpha01"
         const val constraintLayout = "2.0.4"
         const val lifecycle = "2.4.0"
         const val navigation = "2.4.0-beta02"
-        const val work = "2.7.0"
+        const val work = "2.7.1"
         const val hilt = "1.0.0"
 
         object test {
