97e403b8cb972dd10bb2547a269883037d47e2be
Yang <ychescale9@gmail.com>
2022-04-14 01:22:28 +1000

    7.3.0-alpha08

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
@@ -4,7 +4,7 @@ package io.github.reactivecircus.streamlined
 
 object Versions {
     const val kotlin = "1.6.20"
-    const val androidGradlePlugin = "7.3.0-alpha07"
+    const val androidGradlePlugin = "7.3.0-alpha08"
     const val appVersioning = "1.1.0"
     const val googleServices = "4.3.10"
     const val detekt = "1.19.0"
