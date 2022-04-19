35e6d7e117361cfc1c44816fbfbac434c7c27547
Yang <ychescale9@gmail.com>
2022-04-20 01:42:56 +1000

    Kotlin 1.6.21.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
@@ -3,7 +3,7 @@
 package io.github.reactivecircus.streamlined
 
 object Versions {
-    const val kotlin = "1.6.20"
+    const val kotlin = "1.6.21"
     const val androidGradlePlugin = "7.3.0-alpha08"
     const val appVersioning = "1.1.0"
     const val googleServices = "4.3.10"
