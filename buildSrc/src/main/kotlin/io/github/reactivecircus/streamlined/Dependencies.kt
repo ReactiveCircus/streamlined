f40eda1dd113baef1b21e360edef363e8cd36302
Yang <ychescale9@gmail.com>
2022-04-24 15:56:50 +1000

    Test with AGP 7.3.0-alpha09.

--- buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/github/reactivecircus/streamlined/Dependencies.kt
@@ -4,7 +4,7 @@ package io.github.reactivecircus.streamlined
 
 object Versions {
     const val kotlin = "1.6.20"
-    const val androidGradlePlugin = "7.3.0-alpha08"
+    const val androidGradlePlugin = "7.3.0-alpha09"
     const val appVersioning = "1.1.2"
     const val googleServices = "4.3.10"
     const val detekt = "1.20.0"
